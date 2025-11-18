d058d1706e3b38c07ab522433fd896785b0c38f8
mkrtichkar <122966734+mkrtichkar@users.noreply.github.com>
2025-11-18 15:09:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import requests
 import gspread
@@ -152,4 +153,5 @@ def test():
         return f"Table connection ERROR: {e}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
